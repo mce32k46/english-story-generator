7d58c6b590d3edebd48b792fdac5216da35e1166
mce32k46 <andreas-michael.ott@stud.uni-bamberg.de>
2025-10-19 18:44:13 +0200

    Update KI_Agent_english_story_generator.py

--- KI_Agent_english_story_generator.py
+++ KI_Agent_english_story_generator.py
@@ -28,7 +28,7 @@ prompt_values = {
 # ### LLM-Setup
 # --------------------
 import os
-from langchain.chat_models import ChatOpenAI 
+from langchain.chat_models.openai import ChatOpenAI 
 from dotenv import load_dotenv
 load_dotenv()
 
